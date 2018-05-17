c75aacaa0b1fd0d2f4da1f94c1f6cf9823c9fd6f
robrobrob <robbiegabbie@gmail.com>
2018-05-17 09:33:47 -0400

    Initial commit

---
+++ src/Cakes.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Cakes {
+    ArrayList<Filling> fillings = new ArrayList<Filling>();
+    Frosting frosting;
+    int layers;
+    double area;
+    double
+
+    public Cakes(ArrayList<Filling> fillings, Frosting frosting, int layers){
+
+    }
+
+    @Override
+    public String toString() {
+        return "The cake is a lie.";
+    }
+}
---
+++ src/Frosting.java
@@ -0,0 +1,2 @@
+public class Frosting {
+}
